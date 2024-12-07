e2a6f56aa489b8a47af94e880e6ef8241aa83501
Roshan <chyroshan066@gmail.com>
2024-12-07 12:00:11 +0545

    Lab-1 Task-1 done

---
+++ Lab1/task1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+/* Using binet's formula;
+   Logic-> fibo(n) = ((phi^n) - ((1-phi)^n)) / sqrt(5)
+           where, phi = (1 + sqrt(5)) / 2
+*/
+long long Fibo(int n)
+{
+    double goldenRatio = (1 + sqrt(5)) / 2;
+    double fib = (pow(goldenRatio, n) - pow((1 - goldenRatio), n)) / sqrt(5);
+    return round(fib);
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the position of fibonacci term you want to find: ";
+    cin >> n;
+    cout << "The Fibonacci term at position " << n << " is " << Fibo(n);
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# data-structures-and-algorithms
+# Data Sturctures and Algorithms
+
+Solutions of all the labsheet problems
+
+## Lab-1
+
+- **Task-1:** Create and implement a constant time algorithm for computing nth Fibonacci term
